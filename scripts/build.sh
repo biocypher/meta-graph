9e0268f91f5b99a21b519be897913a7373b76c7c
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2023-06-13 18:34:07 +0200

    remove rm command in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,6 @@
 cd /usr/app/
 cp -r /src/* .
 cp config/biocypher_docker_config.yaml config/biocypher_config.yaml
-rm -rf data
 poetry install
 python3 create_knowledge_graph.py
 chmod -R 777 biocypher-log
